8817afa5a9a183eaed494106bd83fc6a8b44d8b4
FWiiii <843189132@qq.com>
2022-06-26 17:34:00 +0800

    delete proxy

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import TelegramBot from 'node-telegram-bot-api';
 const token = '5557629531:AAEsX5ILQJf-dZ-wUJXxecmfYohokjQuAxY';
 const bot = new TelegramBot(token, {
   polling: true,
-  baseApiUrl:"https://tg.hjzovo.workers.dev"
 });
 
 // 匹配/hentai
